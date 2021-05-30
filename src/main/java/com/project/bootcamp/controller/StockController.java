22070cf06ad1b308287fcba65fc779b59630e41c
Thiago <thiagoalves060@gmail.com>
2021-05-30 17:55:40 -0300

    Atualização Final

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -2,6 +2,8 @@ package com.project.bootcamp.controller;
 
 import com.project.bootcamp.model.dto.StockDTO;
 
+import com.project.bootcamp.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,54 +13,55 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin //Permissao do acesso externo para o Front end CrossOrigin
 @RestController
 @RequestMapping(value = "/stock")
 public class StockController {
 
+    @Autowired
+    private StockService service;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto){
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.save(dto));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto){
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.update(dto));
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StockDTO>> findAll(){
-        List<StockDTO> list = new ArrayList<>();
+     /*   List<StockDTO> list = new ArrayList<>();
         StockDTO dto = new StockDTO();
         dto.setId(1L);
         dto.setName("Magazine Luisa");
         dto.setPrice(100D);
         dto.setVariation(10D);
         dto.setDate(LocalDate.now());
-        list.add(dto);
-        return ResponseEntity.ok(list);
+        list.add(dto);*/
+        return ResponseEntity.ok(service.findAll());
     }
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<StockDTO> findById(@PathVariable Long id){
-        List<StockDTO> list = new ArrayList<>();
-        StockDTO stock1 = new StockDTO();
-        stock1.setId(1L);
-        stock1.setName("Magazine Luisa");
-        stock1.setPrice(100D);
-        stock1.setVariation(10D);
-        stock1.setDate(LocalDate.now());
 
-        StockDTO stock2 = new StockDTO();
-        stock2.setId(2L);
-        stock2.setName("Ponto Frio");
-        stock2.setPrice(200D);
-        stock2.setVariation(5D);
-        stock2.setDate(LocalDate.now());
-        list.add(stock1);
-        list.add(stock2);
 
-        StockDTO select = list.stream().filter(x -> x.getId().compareTo(id) == 0).findFirst().get();
+        return ResponseEntity.ok(service.findById(id));
+    }
 
-        return ResponseEntity.ok(select);
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<StockDTO> delete(@PathVariable Long id){
+
+
+        return ResponseEntity.ok(service.delete(id));
+    }
+
+    @GetMapping(value = "/today", produces = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<List<StockDTO>> findByToday(){
+
+
+        return ResponseEntity.ok(service.findByToday());
     }
 
 
---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+    public static final String NOT_RECORDS_FOUND = "Not records found";
+}
